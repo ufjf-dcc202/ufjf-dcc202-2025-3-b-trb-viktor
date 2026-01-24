db59094ec00c56c75b85fbee2b78ce45868203ff
Viktor Srbek <viktor.srbek@estudante.ufjf.br>
2026-01-24 19:58:41 -0300

    coloquei o robô para ser filho do primeiro quadrado que o for fizer

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const tabuleiro = document.getElementById("tabuleiro");
+criarTabuleiro();
 
-        for(let i=0; i<100; i++){
-            const novoDiv = document.createElement("div");
-            novoDiv.classList.add("quadrado");
-            tabuleiro.appendChild(novoDiv);
-        }
+function criarTabuleiro() {
+    const tabuleiro = document.getElementById("tabuleiro");
+
+    for(let i=0; i<100; i++){
+        const novoDiv = document.createElement("div");
+        novoDiv.classList.add("quadrado");
+        tabuleiro.appendChild(novoDiv);
+    }
+
+    const robo = document.getElementById("robo");
+    const primeiroQuadrado = tabuleiro.firstChild;
+    primeiroQuadrado.appendChild(robo);
+}
